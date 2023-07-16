c832dcbfe6445b7aec1a3ac9636b323dcec2826d
Ray259 <elninothepea@gmail.com>
2023-07-16 15:58:44 +0700

    add authentication check and update route

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const {Product}= require('../models');
+const {user} = require('../models');
 
-router.post('/',async (req, res)=> {
+
+//See users list
+router.get('/', async (req, res) => {
+    const userlist = await user.findAll();
+    res.json(userlist);
+});
+
+
+//Add product
+router.post('/productlist',async (req, res)=> {
     const productadd = req.body;
     await Product.create(productadd);
     res.json(productadd);
 })
 
-router.get('/list', async (req, res) => {
+
+//See product list
+router.get('/productlist', async (req, res) => {
     const productlist = await Product.findAll();
     res.json(productlist);
 });
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,25 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {Cart} = require('../models');
-const { Op } = require("sequelize");
+const {Authenticated, Authorized} = require('../middlewares/auth.js');
 
-router.get("/userID=:uid", async (req, res) => {
-    const uid = req.params.uid;
-    const userCart= await Cart.findAll({ where: { userID: uid}});
+
+//Display current cart information by UID
+router.get("/userID=:id", Authenticated, async (req, res) => {
+    const id = req.params.id;
+    const userCart= await Cart.findAll({ where: { userID: id}});
     res.json(userCart);
 })
 
 
+//Delete from cart
 
-router.post("/", async (req, res)=> {
-    const addcart = req.body;
-    await Cart.create(addcart);
-    res.json(addcart);
-})
-
-
-router.get("/test", async (req, res)=> {
-    res.json("test");
-})
 
 module.exports = router;
