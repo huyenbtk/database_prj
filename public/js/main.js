e9397cd635d9c4bb97451095b48f8af1e4509db3
huyenbtk <khanhhuyensthp@gmail.com>
2023-07-17 10:13:02 +0700

    modified signup.html and add signup.js

--- public/js/main.js
+++ public/js/main.js
@@ -93,41 +93,6 @@
             })
     });
 
-    // Sign up //
-    document.getElementById('signup-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        var userName = document.getElementById('user-name').value;
-        var email = document.getElementById('email').value;
-        var fullName = document.getElementById('full-name').value;
-        var phone = document.getElementById('phone').value;
-        var password = document.getElementById('password').value;
-        var confirmPassword = document.getElementById('password_cf').value;
-
-        if (password !== confirmPassword) {
-            var errorMessage = document.getElementById('error-message-cfpw');
-            errorMessage.textContent = 'Mật khẩu không khớp. Vui lòng kiểm tra lại.';
-            errorMessage.style.display = 'block';
-            return;
-        }
-
-        var data = {
-            userName: userName,
-            email: email,
-            fullName: fullName,
-            phone: phone,
-            password: password
-        };
-
-        axios.post('/users/signup', data)
-            .then(function (response) {
-                console.log(response.data);
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
-    });
-
     // SearchProductName //
     document.getElementById("searchForm").addEventListener("submit", function (event) {
         event.preventDefault(); // Ngăn chặn hành vi mặc định của form
---
+++ public/js/signup.js
@@ -0,0 +1,33 @@
+document.getElementById('signup-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var userName = document.getElementById('user-name').value;
+    var email = document.getElementById('email').value;
+    var fullName = document.getElementById('full-name').value;
+    var phone = document.getElementById('phone').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('password-cf').value;
+
+    if (password !== confirmPassword) {
+        var errorMessage = document.getElementById('error-message-cfpw');
+        errorMessage.textContent = 'Please check Confirm Password.';
+        errorMessage.style.display = 'block';
+        return;
+    }
+
+    var data = {
+        userName: userName,
+        email: email,
+        fullName: fullName,
+        phone: phone,
+        password: password
+    };
+
+    axios.post('/users/signup', data)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+});
