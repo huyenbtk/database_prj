8358242f260f2753243af7ff24f20c603c25c4cd
huyenbtk <khanhhuyensthp@gmail.com>
2023-07-17 10:33:00 +0700

    updated signup.js and searchProductName

--- public/js/main.js
+++ public/js/main.js
@@ -93,37 +93,7 @@
             })
     });
 
-    // SearchProductName //
-    document.getElementById("searchForm").addEventListener("submit", function (event) {
-        event.preventDefault(); // Ngăn chặn hành vi mặc định của form
-
-        // Đọc dữ liệu từ người dùng nhập vào
-        var productName = document.getElementById("searchInput").value;
-
-        // Kiểm tra nếu giá trị tìm kiếm trống, không làm gì cả
-        if (!productName.trim()) {
-            return;
-        }
-
-        // Gửi productName đến server và xử lý phản hồi
-        async function sendDataToServer(productName) {
-            try {
-                const response = await axios.post('/menu', { productName });
-                const jsonData = response.data;
-                // Do something with the JSON data
-                console.log(jsonData);
-                window.location.href = "http://localhost:8080/menu/searchProductName=" + productName;
-                // Chuyển đổi dữ liệu JSON thành một mảng đối tượng
-                // const menuData = JSON.parse(response.data);
-            }
-            catch (error) {
-                console.error(error);
-            };
-        };
-    });
-
-
-
+    
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
---
+++ public/js/searchProductName.js
@@ -0,0 +1,31 @@
+// SearchProductName //
+document.getElementById("searchForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định của form
+
+    // Đọc dữ liệu từ người dùng nhập vào
+    var productName = document.getElementById("searchInput").value;
+
+    // Kiểm tra nếu giá trị tìm kiếm trống, không làm gì cả
+    if (!productName.trim()) {
+        return;
+    }
+
+    // Gửi productName đến server và xử lý phản hồi
+    async function sendDataToServer(productName) {
+        try {
+            const response = await axios.post('/menu', { productName });
+            const jsonData = response.data;
+            // Do something with the JSON data
+            console.log(jsonData);
+            window.location.href = "http://localhost:8080/";
+            // Chuyển đổi dữ liệu JSON thành một mảng đối tượng
+            // const menuData = JSON.parse(response.data);
+        }
+        catch (error) {
+            console.error(error);
+            var errorMessage = document.getElementById('error-message-search');
+                errorMessage.textContent = 'Have not';
+                errorMessage.style.display = 'block';
+        };
+    };
+});
